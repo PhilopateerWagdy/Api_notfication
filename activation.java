ced7b348dfb4eebd42fa32e279d98fb96d12c6ea
hazemt1 <45517271+hazemt1@users.noreply.github.com>
2020-11-26 22:00:51 +0200

    Hazem's Part
    Co-Authored-By: yousef essam  <43252073+yousefessam-cs@users.noreply.github.com>

--- activation.java
+++ activation.java
@@ -2,7 +2,7 @@ public class activation extends Content {
 
 	public String Massage() {
 		// TODO - implement activation.Massage
-		throw new UnsupportedOperationException();
+		return " Dear {x} , thank you for registration , your account is activated :)";
 	}
 
 }
